f680a82696d691b320af5b4cc4d26a3f23e1c966
Luis Aramayo <luis.aramayo.18@gmail.com>
2025-02-24 00:01:41 -0300

    termino de corregir configuracion inicial

--- src/Components/Hooks/useAxios.js
+++ src/Components/Hooks/useAxios.js
@@ -3,21 +3,11 @@ import { useAuth } from "./useAuth";
 
 export const useAxios = () => {
   const { userData } = useAuth();
-  const API_ENABLED = false;
-
-  if (!API_ENABLED) {
-    return {
-      get: async () => Promise.resolve({ data: [] }),
-      post: async () => Promise.resolve({ data: {} }),
-      put: async () => Promise.resolve({ data: {} }),
-      delete: async () => Promise.resolve({ data: {} }),
-    };
-  }
 
   const api = axios.create({
     baseURL: process.env.REACT_APP_BASE_URL,
     headers: userData.token ? { Authorization: `Token ${userData.token}` } : {},
   });
 
-  return api
+  return api;
 };
