01e50dab310766d8b82aed1766f88f6aa242c5aa
Luis Aramayo <luis.aramayo.18@gmail.com>
2025-02-23 22:54:42 -0300

    agrego archivo de api enbaled para ver si carga correctamente el frontend

--- src/Components/Hooks/useAxios.js
+++ src/Components/Hooks/useAxios.js
@@ -3,6 +3,16 @@ import { useAuth } from "./useAuth";
 
 export const useAxios = () => {
   const { userData } = useAuth();
+  const API_ENABLED = false;
+
+  if (!API_ENABLED) {
+    return {
+      get: async () => Promise.resolve({ data: [] }),
+      post: async () => Promise.resolve({ data: {} }),
+      put: async () => Promise.resolve({ data: {} }),
+      delete: async () => Promise.resolve({ data: {} }),
+    };
+  }
 
   const api = axios.create({
     baseURL: process.env.REACT_APP_BASE_URL,
