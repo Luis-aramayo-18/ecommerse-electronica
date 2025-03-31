b81a7cf3447850efa830a5291cc5190f87032dbc
luis <luisss.18@hotmail.com>
2025-03-31 20:56:28 -0300

    agrego dom de base para conexion

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -43,7 +43,8 @@ const Nav = () => {
     const getCategories = async () => {
       try {
         const response = await api.get("/categories/");
-
+        console.log(response.data);
+        
         setCategories(response.data);
       } catch (error) {
         console.log(error);
